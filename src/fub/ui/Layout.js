65c99129004a4f04c4f32414ca5c452ccf200db5
ihorbryk <ihorbryk@gmail.com>
2020-12-04 16:36:32 +0200

    Update Layout styles. Add background color

--- src/fub/ui/Layout.js
+++ src/fub/ui/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Layout(props) {
   return (
-    <div className="container mx-auto">
-      <div>
-        <h1 className="text-3xl font-semybold mt-12">{props.title}</h1>
+    <div className="bg-gray-100 h-screen">
+      <div className="container mx-auto">
+        <div>
+          <h1 className="text-3xl font-semybold pt-12">{props.title}</h1>
+        </div>
+        <div className="mt-6">{props.children}</div>
       </div>
-      <div className="mt-6">{props.children}</div>
     </div>
   );
 }
