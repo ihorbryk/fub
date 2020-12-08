50943e287cabf8b496ac68fd6c2e4ceed973b9d8
ihorbryk <ihorbryk@gmail.com>
2020-12-08 09:56:09 +0200

    Implement queryParams for route tool

--- src/fub/tool/route.js
+++ src/fub/tool/route.js
@@ -1,5 +1,5 @@
-// TODO: implement queryParams
 export function url(path, pathParams, queryParams) {
+  console.log(path, pathParams, queryParams);
   const urlParts = path.split("/");
 
   const newUrlParts = urlParts.map((element) => {
@@ -9,5 +9,17 @@ export function url(path, pathParams, queryParams) {
     return element;
   });
 
-  return newUrlParts.join("/");
+  const urlString = newUrlParts.join("/");
+
+  if (!queryParams) {
+    return urlString;
+  }
+
+  const queryArray = Object.keys(queryParams).map((key) => {
+    return `${key}=${queryParams[key]}`;
+  });
+
+  const queryString = queryArray.join("&");
+
+  return urlString + "?" + queryString;
 }
