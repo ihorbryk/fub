e80353ea0b895f19d9b6080fc5429e2d1549b8df
ihorbryk <ihorbryk@gmail.com>
2020-12-11 21:32:08 +0200

    Add AppContext

--- src/fub/Fub.js
+++ src/fub/Fub.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import App from "./classes/App";
 
+export const AppContext = React.createContext();
+
 export default function Fub(props) {
   const getAppInstance = () => {
     if (props.customAppClass) {
@@ -13,14 +15,18 @@ export default function Fub(props) {
 
   const appInstance = getAppInstance();
 
+  const app = {};
+
   return (
-    <Router>
-      <Switch>
-        {appInstance.add()}
-        {appInstance.edit()}
-        {appInstance.list()}
-        {appInstance.home()}
-      </Switch>
-    </Router>
+    <AppContext.Provider value={app}>
+      <Router>
+        <Switch>
+          {appInstance.add()}
+          {appInstance.edit()}
+          {appInstance.list()}
+          {appInstance.home()}
+        </Switch>
+      </Router>
+    </AppContext.Provider>
   );
 }
