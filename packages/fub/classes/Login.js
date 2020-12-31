35530ecf133d5dc18935f0d07fc513c7a19b7677
ihorbryk <ihorbryk@gmail.com>
2020-12-31 17:08:40 +0200

    #8 Update layout style

--- packages/fub/classes/Login.js
+++ packages/fub/classes/Login.js
@@ -2,7 +2,13 @@ import React from "react";
 import LoginTemplate from "../ui/pages/Login";
 
 export default class Login {
+  // Text displayed above login form
+  pageTitle = "Sign in to your account";
+
+  // TODO: Make fields saparate from display template.
+  fields = [];
+
   render({ onLogin }) {
-    return <LoginTemplate onLogin={onLogin} />;
+    return <LoginTemplate onLogin={onLogin} pageTitle={this.pageTitle} />;
   }
 }
--- packages/fub/ui/pages/Login.js
+++ packages/fub/ui/pages/Login.js
@@ -4,7 +4,10 @@ import Form, { Input } from "../Form";
 export default function Login(props) {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="p-4 border rounded-lg shadow bg-white">
+      <div>
+        <h1 className="text-center font-bold text-4xl mb-8">
+          {props.pageTitle}
+        </h1>
         <Form
           initValues={{
             email: "",
@@ -15,36 +18,39 @@ export default function Login(props) {
           }}
         >
           {({ values, handleFieldChange, handleFormSubmit }) => (
-            <div>
-              <form onSubmit={handleFormSubmit} className="w-64">
-                <h1 className="text-center font-bold text-2xl mb-4">Login</h1>
-                <label htmlFor="email">Email</label>
-                <Input
-                  name="email"
-                  type="email"
-                  id="email"
-                  value={values.email}
-                  onChange={(e) =>
-                    handleFieldChange(e.target.name, e.target.value)
-                  }
-                  required
-                />
-                <label htmlFor="password">Password</label>
-                <Input
-                  name="password"
-                  type="password"
-                  id="password"
-                  value={values.password}
-                  onChange={(e) =>
-                    handleFieldChange(e.target.name, e.target.value)
-                  }
-                  required
-                />
+            <div className="p-8 border rounded-lg shadow bg-white">
+              <form onSubmit={handleFormSubmit} className="min-w-64">
+                <div className="mb-4">
+                  <label htmlFor="email">Email</label>
+                  <Input
+                    name="email"
+                    type="email"
+                    id="email"
+                    value={values.email}
+                    onChange={(e) =>
+                      handleFieldChange(e.target.name, e.target.value)
+                    }
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="password">Password</label>
+                  <Input
+                    name="password"
+                    type="password"
+                    id="password"
+                    value={values.password}
+                    onChange={(e) =>
+                      handleFieldChange(e.target.name, e.target.value)
+                    }
+                    required
+                  />
+                </div>
                 <button
                   type="sumbit"
-                  className="px-3 py-2 bg-indigo-500 hover:bg-indigo-600 w-full text-white mt-4 rounded-lg focus:outline-none"
+                  className="text-sm px-3 py-3 bg-indigo-500 hover:bg-indigo-600 w-full text-white mt-4 rounded-lg focus:outline-none"
                 >
-                  Login
+                  Sing in
                 </button>
               </form>
             </div>
